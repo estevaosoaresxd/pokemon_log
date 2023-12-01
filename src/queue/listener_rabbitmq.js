9ea348ecaa266d2b3d553b4e5ceb3643f71a027a
Estevão Soares <estevao.silva@4innovation.com.br>
2023-11-30 22:37:50 -0300

    feat: remove log

--- src/queue/listener_rabbitmq.js
+++ src/queue/listener_rabbitmq.js
@@ -7,21 +7,17 @@ require("dotenv/config");
 const consumer = (exchange, queueName, routerKey, ACK) => {
   rabbitmq.connect(process.env.RABBIT_MQ_URL, (connectError, connection) => {
     if (connectError) {
-      console.log("Erro ao conectar ao RabbitMQ!");
       throw connectError;
     }
 
     connection.createChannel((channelError, channel) => {
       if (channelError) {
-        console.log("Erro ao conectar ao Canal!");
         throw channelError;
       }
 
       channel.assertExchange(exchange, "direct", { durable: true });
 
       channel.assertQueue(queueName, { exclusive: false }, (error, q) => {
-        console.log(" Ouvinte: %s. escutando...", q.queue);
-
         channel.bindQueue(q.queue, exchange, routerKey);
 
         channel.consume(
@@ -30,7 +26,6 @@ const consumer = (exchange, queueName, routerKey, ACK) => {
             ACK(JSON.parse(msg.content));
 
             ON_DEATH((signal, error) => {
-              console.log("Ouvinte %s deixando de escutar!", q.queue);
               setTimeout(() => {
                 connection.close();
                 process.emit(0);
